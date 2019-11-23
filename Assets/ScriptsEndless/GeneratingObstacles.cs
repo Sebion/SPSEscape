2c9902f5e1645629369c578ced3d23293b10864a
SebiNo <hreskosebastian@gmail.com>
2019-11-23 20:04:32 +0100

    randomising gaps and sizes of platforms

--- Assets/ScriptsEndless/GeneratingObstacles.cs
+++ Assets/ScriptsEndless/GeneratingObstacles.cs
@@ -1,24 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering;
 
 public class GeneratingObstacles : MonoBehaviour
 {
-    public GameObject obstacle;
     public Transform generationPoint;
-    public ObjectPooler objectPooler;
+    public ObjectPooler[] objectPools;
+
     private float gapWidth;
     public float gapWidthMax;
     public float gapWidthMin;
+
     public int positionZ;
+
     private float obstacleWidth;
+    private int obstacleSelector;
+    private float[] obstacleWidths;
 
     // Start is called before the first frame update
     void Start()
     {
-        if (!obstacle.GetComponent<BoxCollider2D>().Equals(null))
+        obstacleWidths = new float[objectPools.Length];
+        for (int i = 0; i < objectPools.Length; i++)
         {
-            obstacleWidth = obstacle.GetComponent<BoxCollider2D>().size.x;
+            obstacleWidths[i] = objectPools[i].pooledObject.GetComponent<BoxCollider2D>().size.x;
         }
     }
 
@@ -28,13 +34,20 @@ public class GeneratingObstacles : MonoBehaviour
         if (transform.position.x < generationPoint.transform.position.x)
         {
             gapWidth = Random.Range(gapWidthMin, gapWidthMax);
-            transform.position = new Vector3(transform.position.x + gapWidth + obstacleWidth, transform.position.y,
+            obstacleSelector = Random.Range(0, objectPools.Length);
+            transform.position = new Vector3(transform.position.x + gapWidth + (obstacleWidths[obstacleSelector]/2),
+                transform.position.y,
                 positionZ);
-            // Instantiate(obstacle, transform.position, transform.rotation);
-            GameObject newObstacle = objectPooler.getPooledObject();
+
+
+            GameObject newObstacle = objectPools[obstacleSelector].getPooledObject();
             newObstacle.transform.position = transform.position;
             newObstacle.transform.rotation = transform.rotation;
             newObstacle.SetActive(true);
+            
+            transform.position = new Vector3(transform.position.x + gapWidth + (obstacleWidths[obstacleSelector]/2),
+                transform.position.y,
+                positionZ);
         }
     }
 }
