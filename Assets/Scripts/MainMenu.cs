004faf07e412456804940f89fecfcd38c97a9a15
SebiNo <hreskosebastian@gmail.com>
2019-12-16 19:54:23 +0100

    settings menu working (can change volume of music and fx seperatly)

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,30 +1,42 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Slider MusicSlider;
+    public Slider FxSlider;
+
+
+    private void Start()
+    {
+        SetVolume();
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        
     }
-    
+
     public void PlayEndlessGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
-        
     }
 
-    
 
     public void Quit()
     {
         Application.Quit();
     }
 
-
+    public void SetVolume()
+    {
+        MusicSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+        FxSlider.value = PlayerPrefs.GetFloat("FxVolume");
+    }
 }
---
+++ Assets/ScriptsEndless/VolumeSettings.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class VolumeSettings : MonoBehaviour
+{
+    public AudioMixer Mixer;
+
+    public void setVolumeMainMenu(float sliderValue)
+    {
+        Mixer.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("MusicVolume", sliderValue);
+    }
+
+
+    public void setVolumeFX(float sliderValue)
+    {
+        Mixer.SetFloat("FxVol", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("FxVolume", sliderValue);
+    }
+}
